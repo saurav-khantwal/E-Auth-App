8d829b26229a68b44d0276b61f085dee6a7107f1
saurav-khantwal <souravkhantwal100@gmail.com>
2022-05-11 14:15:37 +0530

    added image feature

--- MAIN_APP/forms.py
+++ MAIN_APP/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, FileField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 
 
@@ -8,6 +8,7 @@ class Register_form(FlaskForm):
     email_address = StringField(label='Email address', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm password', validators=[EqualTo('password1'), DataRequired()])
+    image = FileField(label="Add image")
     submit = SubmitField(label='Create Account')
 
 
